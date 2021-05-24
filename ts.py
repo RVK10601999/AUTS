9184576f06dd99ded058091b6ca90c085a952cdd
RVK10601999 <62978278+RVK10601999@users.noreply.github.com>
2021-05-24 21:02:31 +0530

    Update ts.py

--- ts.py
+++ ts.py
@@ -19,7 +19,10 @@ def err(x,y):
         mse = mse.mean()
         a = [bias,mae,mape,mse]
         return a
+adfvl = 0
+from statsmodels.tsa.stattools import adfuller
 def adfuller_test(data):
+        global adfvl
         result=adfuller(data)
         labels = ['ADF Test Statistic','p-value','Number of Lags Used','Number of Observations Used']
         for value,label in zip(result,labels):
@@ -28,10 +31,28 @@ def adfuller_test(data):
             st.write("strong evidence against the null hypothesis(Ho), reject the null hypothesis. Data has no unit root and is stationary")
         else:
             st.write("weak evidence against null hypothesis, time series has a unit root, indicating it is non-stationary ")
+            adfvl = 1
+
+from statsmodels.tsa.stattools import kpss
+def kpss_test(series, **kw):
+    global adfvl
+    statistic, p_value, n_lags, critical_values = kpss(series, **kw)
+    # Format Output
+    st.write(f'KPSS Statistic: {statistic}')
+    st.write(f'p-value: {p_value}')
+    st.write(f'num lags: {n_lags}')
+    st.write('Critial Values:')
+    for key, value in critical_values.items():
+        st.write(f'   {key} : {value}')
+    if p_value<.05:
+        st.write('Not stationary')
+        adfvl += 1
+    else:
+        st.write('stationary')
+    return adfvl
 def decompose_series(ts):
     '''
     This function applies a seasonal decomposition to a time series. It will generate a season plot, a trending plot, and, finally, a resid plot
-
     Args.
         ts (Pandas Series): a time series to be decomposed
     '''
@@ -81,81 +102,123 @@ st.sidebar.write('           ')
 st.sidebar.write('           ')
 st.sidebar.write('           ')
 st.sidebar.write('           ')
-
 if uploaded_files:
-    for file in uploaded_files:
-        file.seek(0)
-    if str(file.name).endswith('v'):
-        uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
-    else:
-        uploaded_data_read = [pd.read_excel(file) for file in uploaded_files]
-    df = pd.concat(uploaded_data_read)
-    st.write(df)
-    df = df[df.iloc[:,1].notna()]
-    st.write('File Chosen')
-    st.write('Missing valus are eliminated')
-    df.columns = ['time','series']
-    df['series'] = df['series'].astype('float')
-    st.write('Columns Aligned')
-    df.reset_index(inplace = True) 
-    st.write('Index reset')
-    df.index = pd.to_datetime(df['time'])
-    df.drop(columns = ['index','time'],inplace = True)
-    st.write('Unnecessary Columns are removed')
-    st.write(df)
-    decompose_series(df)
-    st.write('Successfully Generated Decomposition plot.')
-    from statsmodels.tsa.stattools import adfuller
-    adfuller_test(df)
-    st.write(10)
-    st.write(1)
-    fig = plt.figure(figsize=(12,8))
-    ax1 = fig.add_subplot(211)
-    fig = plot_acf(df['series'].iloc[12:],ax=ax1)
-    ax2 = fig.add_subplot(212)
-    fig = plot_pacf(df['series'].iloc[12:],lags=40,ax=ax2)
-    st.pyplot()
-    lst = []
-    mods = []
-    bstmd = 0
-    bstord = []
-    for i in range(int(pi),int(pe)+1):
-      for j in range(int(di),int(de)+1):
-        for k in range(int(qi),int(qe)+1):
-          ar = ARIMA(endog=df['series'],order=(i,j,k)).fit(disp=False)
-          mods.append(ar)
-          st.write(i,j,k)
-          if bstmd==0:
-            bstmd = ar
-            bstord = (i,j,k)
-            erl = err(df['series'],ar.predict())
-          else:
-            cnt = 0
-            erl1 = err(df['series'],ar.predict())
-            for z in range(0,len(erl1)):
-              if erl1[z] < erl[z]:
-                cnt += 1
-            if cnt>3:
-              bstmd = ar
-              bstord = (i,j,k)
-    st.write('Best Order is found......')
-    st.write(bstord)
-    st.write('Plotting actual values and predicted values......')
-    df.plot(figsize=(25,8))
-    bstmd.predict().plot(figsize=(25,8))
-    st.pyplot()
-    ind6 = abs(int(len(df)/6))
-    ind46 = abs(int(len(df)*4/6))
-    df['prd101'] = None
-    df['act'] = None
-    df['prd101'].iloc[ind6:ind46] = bstmd.predict().iloc[ind6:ind46]
-    df['act'].iloc[:ind6] = df['series'].iloc[:ind6]
-    df['act'].iloc[ind46:] = df['series'].iloc[ind46:]
-    st.write('Plotting A concatenated plot of Actual and Predicted values........')
-    df[['act','prd101']].plot(figsize=(25,8))
-    st.pyplot()
-    strt = st.text_input('Enter the initial month to predict.......')
-    end = st.text_input('Enter the final month to predict.......')
-    btns = st.button('Predict')
-    if strt and btns and end:
-        st.write(bstmd.predict(int(strt)-1,int(end)-1))
+        for file in uploaded_files:
+            file.seek(0)
+            if str(file.name).endswith('v'):
+                uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
+            else:
+                uploaded_data_read = [pd.read_excel(file) for file in uploaded_files]
+            df = pd.concat(uploaded_data_read)
+            st.write(df)
+            df = df[df.iloc[:,1].notna()]
+            st.write('File Chosen')
+            st.write('Missing valus are eliminated')
+            df.columns = ['time','series']
+            df['series'] = df['series'].astype('float')
+            st.write('Columns Aligned')
+            df.reset_index(inplace = True) 
+            st.write('Index reset')
+            df.index = pd.to_datetime(df['time'])
+            df.drop(columns = ['index','time'],inplace = True)
+            st.write('Unnecessary Columns are removed')
+            st.write(df)
+            decompose_series(df)
+            st.write('Successfully Generated Decomposition plot.')
+            adfuller_test(df)
+            kpss_test(df)
+#Non-Stationary 
+            if adfvl>0:
+                st.write(11)
+                st.write(2)
+                fig = plt.figure(figsize=(12,8))
+                ax1 = fig.add_subplot(211)
+                fig = plot_acf(df['series'].iloc[12:],ax=ax1)
+                ax2 = fig.add_subplot(212)
+                fig = plot_pacf(df['series'].iloc[12:],lags=40,ax=ax2)
+                st.pyplot()
+                lst = []
+                mods = []
+                bstmd = 0
+                bstord = []
+                for i in range(int(pi),int(pe)+1):
+                    for j in range(1,3):
+                        for k in range(int(qi),int(qe)+1):
+                            try:
+                                ar = ARIMA(endog=df['series'],order=(i,j,k)).fit(disp=False)
+                                mods.append(ar)
+                                st.write(i,j,k)
+                                if bstmd==0:
+                                    bstmd = ar
+                                    bstord = (i,j,k)
+                                    erl = err(df['series'],ar.predict())
+                                else: 
+                                    cnt = 0
+                                    erl1 = err(df['series'],ar.predict())
+                                    for z in range(0,len(erl1)):
+                                        if erl1[z] < erl[z]:
+                                            cnt += 1
+                                    if cnt>3:
+                                        bstmd = ar
+                                        bstord = (i,j,k)
+                            except: 
+                                continue
+                                st.write('continuing')
+    #stationary
+            else:
+                st.write(11)
+                st.write(2)
+                fig = plt.figure(figsize=(12,8))
+                ax1 = fig.add_subplot(211)
+                fig = plot_acf(df['series'].iloc[12:],ax=ax1)
+                ax2 = fig.add_subplot(212)
+                fig = plot_pacf(df['series'].iloc[12:],lags=40,ax=ax2)
+                st.pyplot()
+                lst = []
+                mods = []
+                bstmd = 0
+                bstord = []
+                for i in range(int(pi),int(pe)+1):
+                    for j in range(int(di),int(de)+1):
+                        for k in range(int(qi),int(qe)+1):
+                            try:
+                                ar = ARIMA(endog=df['series'],order=(i,j,k)).fit(disp=False)
+                                mods.append(ar)
+                                st.write(i,j,k)
+                                if bstmd==0:
+                                    bstmd = ar
+                                    bstord = (i,j,k)
+                                    erl = err(df['series'],ar.predict())
+                                else: 
+                                    cnt = 0
+                                    erl1 = err(df['series'],ar.predict())
+                                    for z in range(0,len(erl1)):
+                                        if erl1[z] < erl[z]:
+                                            cnt += 1
+                                    if cnt>3:
+                                        bstmd = ar
+                                        bstord = (i,j,k)
+                            except: 
+                                continue
+                                st.write('continuing')
+            st.write('Best Order is found......')
+            st.write(bstord)
+            st.write('Plotting actual values and predicted values......')
+            df.plot(figsize=(25,8))
+            bstmd.predict().plot(figsize=(25,8))
+            st.pyplot()
+            ind6 = abs(int(len(df)/6))
+            ind46 = abs(int(len(df)*4/6))
+            df['prd101'] = None
+            df['act'] = None
+            df['prd101'].iloc[ind6:ind46] = bstmd.predict().iloc[ind6:ind46]
+            df['act'].iloc[:ind6] = df['series'].iloc[:ind6]
+            df['act'].iloc[ind46:] = df['series'].iloc[ind46:]
+            st.write('Plotting A concatenated plot of Actual and Predicted values........')
+            df[['act','prd101']].plot(figsize=(25,8))
+            st.pyplot()
+            strt = st.text_input('Enter the initial month to predict.......')
+            end = st.text_input('Enter the final month to predict.......')
+            btns = st.button('Predict')
+            if strt and btns and end:
+                st.write(bstmd.predict(int(strt)-1,int(end)-1))
